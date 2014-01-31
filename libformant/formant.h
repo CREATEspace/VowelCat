cce91ad52c548b7f14b67b1ccf83048846c44abd
Mick Koch <kchmck@gmail.com>
2014-01-30 19:53:27 -0500

    Add some docs

--- libformant/formant.h
+++ libformant/formant.h
@@ -34,20 +34,33 @@ typedef struct {   /* structure to hold raw LPC analysis data */
 } pole_t;
 
 typedef struct sound_t {
+    // Sample rate of the audio data in Hz.
     size_t sample_rate;
+    // Number of channels (1 for mono, 2 for stereo, and so on).
     size_t n_channels;
-    // The number of samples per channel in blocks.
+    // The number of samples per channel in the audio data.
     size_t n_samples;
+    // The audio data itself.
     storage_t *blocks;
 
     pole_t **pole;
 } sound_t;
 
+// Initialize the given sound and fill in its sample rate and number of
+// channels.
 void sound_init(sound_t *s, size_t sample_rate, size_t n_channels);
+
+// Release the memory held by the given sound.
 void sound_destroy(sound_t *s);
+
+// Load a buffer of samples into the given sound.
 void sound_load_samples(sound_t *s, const short *samples, size_t n_samples);
+
+// Calculate the formants for the samples in the given sound. The sound is
+// modified in place.
 void sound_calc_formants(sound_t *s);
 
+// Get the i'th sample in the given channel.
 static inline storage_t sound_get_sample(const sound_t *s, size_t chan, size_t i) {
     return s->blocks[i * s->n_channels + chan];
 }
