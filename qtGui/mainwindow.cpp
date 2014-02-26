db1b488405f4d3584483b8995e2c07549c8b8c5b
Natalie Yarnell <ny143510@ohio.edu>
2014-02-26 16:45:57 -0500

    increasing the size of the symbols

--- qtGui/mainwindow.cpp
+++ qtGui/mainwindow.cpp
@@ -69,79 +69,79 @@ void MainWindow::setupPlot()
   ui->customPlot->addItem(upperHighBackRounded);
   upperHighBackRounded->position->setCoords(750, 295);
   upperHighBackRounded->setText("u");
-  upperHighBackRounded->setFont(QFont(font().family(), 20));
+  upperHighBackRounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerHighBackRounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerHighBackRounded);
   lowerHighBackRounded->position->setCoords(910, 334);
   lowerHighBackRounded->setText("ʊ");
-  lowerHighBackRounded->setFont(QFont(font().family(), 20));
+  lowerHighBackRounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *upperMidBackRounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(upperMidBackRounded);
   upperMidBackRounded->position->setCoords(727, 406);
   upperMidBackRounded->setText("o");
-  upperMidBackRounded->setFont(QFont(font().family(), 20));
+  upperMidBackRounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerMidBackRounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerMidBackRounded);
   lowerMidBackRounded->position->setCoords(830, 541);
   lowerMidBackRounded->setText("ɔ");
-  lowerMidBackRounded->setFont(QFont(font().family(), 20));
+  lowerMidBackRounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerLowBackRounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerLowBackRounded);
   lowerLowBackRounded->position->setCoords(843, 652);
   lowerLowBackRounded->setText("ɒ");
-  lowerLowBackRounded->setFont(QFont(font().family(), 20));
+  lowerLowBackRounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerLowBackUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerLowBackUnrounded);
   lowerLowBackUnrounded->position->setCoords(1065, 781);
   lowerLowBackUnrounded->setText("ɑ");
-  lowerLowBackUnrounded->setFont(QFont(font().family(), 20));
+  lowerLowBackUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerLowCentralUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerLowCentralUnrounded);
   lowerLowCentralUnrounded->position->setCoords(1211, 784);
   lowerLowCentralUnrounded->setText("ä");
-  lowerLowCentralUnrounded->setFont(QFont(font().family(), 20));
+  lowerLowCentralUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerLowFrontUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerLowFrontUnrounded);
   lowerLowFrontUnrounded->position->setCoords(1632, 806);
   lowerLowFrontUnrounded->setText("a");
-  lowerLowFrontUnrounded->setFont(QFont(font().family(), 20));
+  lowerLowFrontUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *upperLowFrontUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(upperLowFrontUnrounded);
   upperLowFrontUnrounded->position->setCoords(1782, 766);
   upperLowFrontUnrounded->setText("æ");
-  upperLowFrontUnrounded->setFont(QFont(font().family(), 20));
+  upperLowFrontUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerMidFrontUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerMidFrontUnrounded);
   lowerMidFrontUnrounded->position->setCoords(1840, 541);
   lowerMidFrontUnrounded->setText("ɛ");
-  lowerMidFrontUnrounded->setFont(QFont(font().family(), 20));
+  lowerMidFrontUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *upperMidFrontUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(upperMidFrontUnrounded);
   upperMidFrontUnrounded->position->setCoords(2148, 434);
   upperMidFrontUnrounded->setText("e");
-  upperMidFrontUnrounded->setFont(QFont(font().family(), 20));
+  upperMidFrontUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *lowerHighFrontUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(lowerHighFrontUnrounded);
   lowerHighFrontUnrounded->position->setCoords(2187, 360);
   lowerHighFrontUnrounded->setText("ɪ");
-  lowerHighFrontUnrounded->setFont(QFont(font().family(), 20));
+  lowerHighFrontUnrounded->setFont(QFont(font().family(), 30));
 
   QCPItemText *upperHighFrontUnrounded = new QCPItemText(ui->customPlot);
   ui->customPlot->addItem(upperHighFrontUnrounded);
   upperHighFrontUnrounded->position->setCoords(2343, 294);
   upperHighFrontUnrounded->setText("i");
-  upperHighFrontUnrounded->setFont(QFont(font().family(), 20));
+  upperHighFrontUnrounded->setFont(QFont(font().family(), 30));
 
   for (size_t i = 0; i < Tracer::COUNT; i += 1)
     tracers[i] = new Tracer(plot, graph, i);
