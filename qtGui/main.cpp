62be7b98384ba0b0df0d2d7a3d64da48b7b5e6ac
Mick Koch <kchmck@gmail.com>
2014-02-26 19:32:10 -0500

    s/a/app/

--- qtGui/main.cpp
+++ qtGui/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
   QApplication::setGraphicsSystem("raster");
 #endif
 
-  QApplication a(argc, argv);
+  QApplication app(argc, argv);
   MainWindow window;
   FormantWorker worker;
 
@@ -21,5 +21,5 @@ int main(int argc, char *argv[])
   window.show();
   worker.start();
 
-  return a.exec();
+  return app.exec();
 }
