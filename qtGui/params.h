8e8eb9b1f4b1b5c07332edab8f44c515a6f80915
Patrick Gray <pg219709@ohio.edu>
2014-04-23 15:43:19 -0400

    Adding parameters file

---
+++ qtGui/params.h
@@ -0,0 +1,13 @@
+// Copyright 2014 Formant Industries. See the Copying file at the top-level
+// directory of this project.
+
+#ifndef PARAMS_H
+#define PARAMS_H
+
+static const size_t SAMPLE_RATE = 11025; //1 << 13;
+static const size_t DOWNSAMPLE_RATE = 1 << 13;
+static const size_t CHANNELS = 1;
+static const size_t FRAMES_PER_CHUNK = 1 << 11;
+static const size_t SAMPLES_PER_CHUNK = FRAMES_PER_CHUNK * CHANNELS;
+
+#endif
