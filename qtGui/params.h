8beae99f8112771411b88cb0270c4bc0c3281a7b
Mick Koch <mick@kochm.co>
2014-04-27 20:58:53 -0400

    Tweak sample rate and frames per chunk
    These values seem to give good latency with no loss in accuracy. Snack
    would normally downsample the sound to 10000Hz anyway

--- qtGui/params.h
+++ qtGui/params.h
@@ -4,9 +4,9 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
-static const size_t SAMPLE_RATE = 11025;
+static const size_t SAMPLE_RATE = 10000;
 static const size_t CHANNELS = 1;
-static const size_t FRAMES_PER_CHUNK = 2048;
+static const size_t FRAMES_PER_CHUNK = 1000;
 static const size_t SAMPLES_PER_CHUNK = FRAMES_PER_CHUNK * CHANNELS;
 
 static const uint32_t F1_MIN = 250;
