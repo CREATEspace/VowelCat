d3bc2ee2a0493b272cf6cd92ebe94f1ceba33ee8
Mick Koch <mick@kochm.co>
2014-04-29 09:11:37 -0400

    Tweak F1_MIN to pick up more eeeee sounds

--- qtGui/params.h
+++ qtGui/params.h
@@ -9,7 +9,7 @@ static const size_t CHANNELS = 1;
 static const size_t FRAMES_PER_CHUNK = 1000;
 static const size_t SAMPLES_PER_CHUNK = FRAMES_PER_CHUNK * CHANNELS;
 
-static const uint32_t F1_MIN = 250;
+static const uint32_t F1_MIN = 150;
 static const uint32_t F1_MAX = 850;
 
 static const uint32_t F2_MIN = 700;
