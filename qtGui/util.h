03938677d369feafbe731cdd152567d6f59af8af
Mick Koch <mick@kochm.co>
2014-04-29 12:50:56 -0400

    Add in util.h
    Derp, sorry guysh

---
+++ qtGui/util.h
@@ -0,0 +1,14 @@
+// Copyright 2014 Formant Industries. See the Copying file at the top-level
+// directory of this project.
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+#define DIV_CEIL(n, d) (((n) + (d) - 1) / (d))
+
+#define ABS(x) ((x) > 0 ? (x) : -(x))
+
+#endif
