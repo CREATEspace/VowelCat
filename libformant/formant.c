13063c2b88cfdcf067c12bc09443ec49b03950a1
Mick Koch <mick@kochm.co>
2014-05-18 15:58:50 -0400

    Fix another possible buffer overflow in fir
    When ncoef >= 129

--- libformant/formant.c
+++ libformant/formant.c
@@ -800,7 +800,7 @@ static void fir(formant_sample_t *samples, const size_t nsamples,
     const size_t lcoef = ncoef - 1;
     const size_t k = (ncoef * 2) - 1;
 
-    formant_sample_t co[lcoef * 2], mem[256];
+    formant_sample_t co[lcoef * 2], mem[ncoef * 2];
 
     size_t in = 0;
     size_t out = 0;
